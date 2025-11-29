584d5f876b09113d211cc27253cf4dff420524c2
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2025-11-29 22:53:40 +0200

    create 2 fun to calc num of rating and the average

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import Sum
 
 # Create your models here.
 
@@ -8,6 +9,21 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Meal(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
+
+    def num_of_ratings(self):
+        ratings = Rating.objects.filter(meal=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        ratings = Rating.objects.filter(meal=self)
+        # note that there is 2 methods to calculate the total
+        # 1st method is using a for loop
+        # 2nd method is using aggregate function(this is more efficient cuz it does it in the database level)
+        # total = sum([rating.stars for rating in ratings])
+        total = ratings.aggregate(Sum("stars"))["stars__sum"]
+        if total is None:
+            return 0
+        return total / len(ratings)
 
     def __str__(self):
         return self.title
